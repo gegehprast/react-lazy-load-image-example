e1f76510656a48e53e3e4039ae5a5f73b826d794
gegehprast <gegeh.prast@gmail.com>
2020-06-23 21:08:57 +0800

    purge config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
+/* eslint-disable no-undef */
 module.exports = {
-  purge: [],
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
+    purge: {
+        enabled: process.env.NODE_ENV === 'production',
+        content: ['./components/**/*.jsx', './pages/**/*.jsx'],
+    },
+    theme: {
+        extend: {},
+    },
+    variants: {},
+    plugins: [],
 }
